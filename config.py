33a7a4a424c5d493a1e2d00b123b97541b688cf4
Diksha goyal <goyaldiksha@gmail.com>
2021-03-03 23:46:02 -0500

    Updated templates and added more routes to work with signup, signin modules using backend api, also worked on the UI part for menu management

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 #code config start
 BRAND_NAME = "Restaurant Managemnet System"
 API_URL = "http://localhost:7000" #This is where API is running on the local server. 
+SECRET_KEY = "ANY_KEY_SET_BY_YOU"
 #code config end
