f78d5123ef6c41fdb69b6866e3dbf7fd97f4860f
ritesh <ritesh.sharma677910@gmail.com>
2021-03-22 23:31:46 -0400

    changed the UI of Menu Management and also changed flask code to be worked with new design of backend that is discussed with the team. Next is waiting for backend to implement the new idea.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-#imports start
+# imports start
 from flask import Flask, jsonify, session
 from flask import render_template, request, flash, redirect, url_for
 from config import *
 import time
 import requests
-#imports end
+
+# imports end
 
 app = Flask(__name__)
 
@@ -16,45 +17,50 @@ def index():
     if check_user_signed_in():
         return redirect(url_for('dashboard'))
     data = {
-        "brandName":BRAND_NAME
+        "brandName": BRAND_NAME
     }
-    return render_template("index.html", data = data)
+    return render_template("index.html", data=data)
+
 
 @app.route('/send/', methods=["POST"])
 def index_send():
     if check_user_signed_in():
         return redirect(url_for('dashboard'))
     data = {
-        "email":request.form["email"],
-        "password":request.form["pass"]
+        "email": request.form["email"],
+        "password": request.form["pass"]
     }
     r = requests.get(API_URL + "/restaurant/", json=data)
     if 'email' in r.json():
-        set_user_signed_in(r.json()['email'])
-        return jsonify({"login":"true"})
-    return jsonify({"login":"false"})
+        set_user_signed_in(r.json()['id'])
+        return jsonify({"login": "true"})
+    return jsonify({"login": "false"})
+
 
 @app.route('/logout/')
 def logout():
     set_user_signed_out()
     return redirect(url_for('index'))
 
+
 @app.route('/onboarding/')
 def onboarding():
     if check_user_signed_in():
         return redirect(url_for('dashboard'))
     data = {
-        "brandName":BRAND_NAME
+        "brandName": BRAND_NAME
     }
-    return render_template("signup.html",data = data)
+    return render_template("signup.html", data=data)
+
+
 @app.route('/onboarding/send/', methods=["POST"])
 def onboarding_send():
     if check_user_signed_in():
         return redirect(url_for('dashboard'))
     data = {
-        "name":request.form['rname'],
-        "email":request.form['email'],
-        "password":request.form["pass"]
+        "name": request.form['rname'],
+        "email": request.form['email'],
+        "password": request.form["pass"]
     }
 
     time.sleep(4)
@@ -69,20 +75,21 @@ def dashboard():
     if not check_user_signed_in():
         return redirect(url_for('index'))
     data = {
-        "brandName":BRAND_NAME, 
-     
+        "brandName": BRAND_NAME,
+
     }
     return render_template("private/dashboard.html", data=data)
 
+
 @app.route('/table-reservation/')
 def table_reservation():
-    if not check_user_signed_in():
-        return redirect(url_for('index'))
+    # if not check_user_signed_in():
+    #     return redirect(url_for('index'))
     data = {
-        "brandName":BRAND_NAME, 
+        "brandName": BRAND_NAME,
 
     }
-    return render_template("private/table-reservation.html", data = data)
+    return render_template("private/table-reservation.html", data=data)
 
 
 @app.route('/menu-management/')
@@ -90,31 +97,43 @@ def menu_management():
     if not check_user_signed_in():
         return redirect(url_for('index'))
 
-    r  = requests.get(API_URL + "/product/", json={"email":get_user_signed_in()})
+    categories = requests.get(API_URL + "/category/", json={"restaurant": get_user_signed_in()})
+    d = categories.json()
     data = {
-        "brandName":BRAND_NAME, 
-        "menu":r.json()
+        "brandName": BRAND_NAME,
 
+        "categories": d
     }
-  
-    return render_template("private/menu-management.html", data = data)
+    return render_template("private/menu-management.html", data=data)
+
+
+@app.route('/menu-management/add-category/', methods=["POST"])
+def menu_management_add_category():
+    category = request.form['category']
+    r = requests.post(API_URL + "/category/", json={"name": category, "restaurant": get_user_signed_in()})
+    return redirect(url_for('menu_management'))
+
 
 # private content ends
 
 
-
-
-
-# Helper Functions start 
+# Helper Functions start
 
 def set_user_signed_in(email):
-    session['email'] = email
+    session['id'] = email
+
+
 def set_user_signed_out():
-    session.pop('email')
+    session.pop('id')
+
+
 def check_user_signed_in():
-    return 'email' in session
+    return 'id' in session
+
+
 def get_user_signed_in():
-    return session['email']
+    return session['id']
+
 
 # Helper functins ends
 
--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 #code config start
 BRAND_NAME = "Restaurant Managemnet System"
-<<<<<<< HEAD
-API_URL = "http://localhost:7000" #This is where API is running on the local server.
 
-=======
+
+
 API_URL = "http://localhost:7000" #This is where API is running on the local server. 
 SECRET_KEY = "ANY_KEY_SET_BY_YOU"
->>>>>>> 33a7a4a424c5d493a1e2d00b123b97541b688cf4
+
 #code config end
