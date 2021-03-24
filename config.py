86db27a937a99e5d686b515bb54bd896e91059ae
Diksha2426 <2426goyal@gmail.com>
2021-03-23 22:05:14 -0400

    updated config file and updated menu-management.html with clean codebase

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
+import os
+
 #code config start
 BRAND_NAME = "Restaurant Managemnet System"
-
-
-
-API_URL = "http://localhost:7000" #This is where API is running on the local server. 
+API_URL = "http://localhost:7000" #This is where API is running on the local server.
 SECRET_KEY = "ANY_KEY_SET_BY_YOU"
-
+UPLOAD_FOLDER = os.getcwd() + os.sep + 'images' + os.sep
+MAX_CONTENT_PATH = 3e+6
 #code config end
